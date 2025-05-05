1495b3650c431ffc40b82ba25a2c5e22987ad14f
kkayali <alikaya20041299@gmail.com>
2025-05-05 11:10:08 +0300

    sensor

---
+++ Sensor/app/src/main/java/com/example/myapplication/MagnetometerActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MagnometerActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor magnometer;
+    private SensorEventListener listener;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.magnometer);
+
+        textView = findViewById(R.id.textView_mag);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //magnometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNOMETER); DONT KNOW HOW
+
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                //content
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, magnometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+    }
+}
